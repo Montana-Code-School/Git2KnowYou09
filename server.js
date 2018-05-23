1b01473f19add7fe03708527bcf4aef0f141648e
Claire O'Brien <claireobrien@Claires-MacBook-Pro.local>
2018-05-23 15:50:03 -0600

    completed all paths for directories

--- server.js
+++ server.js
@@ -11,4 +11,12 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/kate', express.static(path.join(__dirname, 'public/kate')));
 
+app.use('/scott', express.static(path.join(__dirname, 'public/scott')));
+
+app.use('/claire', express.static(path.join(__dirname, 'public/claire')));
+
+app.use('/tyler', express.static(path.join(__dirname, 'public/tyler')));
+
+app.use('/kate', express.static(path.join(__dirname, 'public/joie')));
+
 app.listen(port, function() {console.log("port 3k")});
