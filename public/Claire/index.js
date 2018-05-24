e3a236c7c2f9e2332e622406209353b89e40c625
Claire O'Brien <claireobrien@Claires-MacBook-Pro.local>
2018-05-24 16:41:25 -0600

    making changes to claires files

--- public/Claire/index.js
+++ public/Claire/index.js
@@ -1,18 +1,18 @@
-var request = new XMLHttpRequest();
-
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-request.onload = function () {
-
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      console.log(movie.title);
-    });
-  } else {
-    console.log('error');
-  }
-}
-
-request.send();
+$('#srchButton').click(function() {
+  var s1 = $( "input[name='search1']").val().toLowerCase();
+  var s2 = $( "input[name='search2']").val().toLowerCase();
+  var searchUrl = 'https://pixabay.com/api/?key=9093855-07854ae771d82d6fe28481ea8&q=' + s1 + '+' + s2 + '&image_type=photo';
+  $.ajax({
+    url : searchUrl,
+    success : function(data) {
+       console.log(data);
+       data.hits.map(function(v){
+         var html = '<img src="' + v.webformatURL + '" alt="">';
+         $("#stuff").append(html);
+       });
+    },
+    error : function(err) {
+        console.log(err)
+    }
+  });
+})
